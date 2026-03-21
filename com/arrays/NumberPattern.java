fbbd5fa8ce0a788f1ab48d043c573b39a65ee533
onkarkarande77 <karandeonkar05@gmail.com>
2026-03-21 19:45:54 +0530

    this is the number pattern

---
+++ com/arrays/NumberPattern.java
@@ -0,0 +1,24 @@
+package com.pattern;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= (n - i) * 2; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
