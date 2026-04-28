220da41521ac5d62f6e2122322b12708c7274a4d
onkarkarande77 <karandeonkar05@gmail.com>
2026-04-28 18:19:24 +0530

    this is the code of Product Except Self Arrangment

---
+++ com/dsa/ProductExceptSelf.java
@@ -0,0 +1,30 @@
+package com.dsa;
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Step 1: left product
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: right product
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+        // Output: [24, 12, 8, 6]
+    }
+}
