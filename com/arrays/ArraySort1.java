f17095ff52affabe974909879f132735c5183899
onkarkarande77 <karandeonkar05@gmail.com>
2026-08-02 18:32:03 +0530

    Update ArraySort1.java

--- com/arrays/ArraySort1.java
+++ com/arrays/ArraySort1.java
@@ -11,7 +11,7 @@ public class ArraySort1 {
 			
 		for(int j=0;j<ar.length;j++) {
 			  if(ar[i]<ar[j]) {
-				  int temp=ar[i];
+				  int temp=ar[i]; 
 				     ar[i]=ar[j];
 				     ar[j]=temp;
 				  } 
