15df25180b9822b7f872bfc74d6ac9a0f7dc0e31
onkarkarande77 <karandeonkar05@gmail.com>
2026-08-03 23:48:03 +0530

    Update Calculater.java

--- com/AllBasic/Calculater.java
+++ com/AllBasic/Calculater.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Calculater {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub 
 	 	Scanner sc=new Scanner(System.in);
 	 	System.out.println("Enter the first no ");
