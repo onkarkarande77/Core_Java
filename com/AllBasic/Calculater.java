827d433a1bc0b91119933769d32d7161982e33bd
onkarkarande77 <karandeonkar05@gmail.com>
2026-08-20 19:40:08 +0530

    Update Calculater.java

--- com/AllBasic/Calculater.java
+++ com/AllBasic/Calculater.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Calculater {
   
-	
+	 
 	public static void main(String[] args) {   
 		// TODO Auto-generated method stub 
 	 	Scanner sc=new Scanner(System.in);
