860c41510173d6245b07a805d0e787d1e4b37523
onkarkarande77 <karandeonkar05@gmail.com>
2026-09-05 18:41:26 +0530

    Update RandomOTP.java

--- com/random/RandomOTP.java
+++ com/random/RandomOTP.java
@@ -5,7 +5,7 @@ import java.util.RandomAccess;
 
 public class RandomOTP {
  
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
 		 Random rd=new Random();
 		 String otp="";
